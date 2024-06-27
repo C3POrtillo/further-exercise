016ef1f59fd5a7d84df160ce460f226849d9066c
Camilo III Ortillo <camortillo@gmail.com>
2024-06-27 01:09:50 -0600

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import type { FC } from 'react';
+
+import Form from '@/components/form/Form';
+
+const Home: FC = () => (
+  <body>
+    <Form/>
+  </body>
+ );
+
+export default Home;
