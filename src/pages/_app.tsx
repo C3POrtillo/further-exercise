ce99b01f16360303abe15c5d40824a63fe349a94
Camilo III Ortillo <camortillo@gmail.com>
2024-06-28 18:43:15 -0600

    update format

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import type { AppProps } from 'next/app';
 
-
 import '@/styles/globals.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
 export default MyApp;
